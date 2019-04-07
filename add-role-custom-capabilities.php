0c45e3fd68cf8002b88a29a2b1e7f236eb896339
nabtron <developer@nabtron.com>
2019-04-08 01:20:07 +0500

    Create add-role-custom-capabilities.php

---
+++ add-role-custom-capabilities.php
@@ -0,0 +1,30 @@
+
+add_role('bound360_moderator', 'Bound360 Moderator', array(
+    'read' => true, // True allows that capability
+    'edit_posts' => false,
+	'delete_posts' => false, // Use false to explicitly deny
+	'bound360_moderator' => true,
+));
+
+if( get_role('bound360_moderator') ){
+	remove_role( 'bound360_moderator' );
+	echo "removed";
+	//die();
+}
+
+$result = add_role(
+    'basic_contributor',
+    __( 'Basic Contributor' ),
+    array(
+        'read'         => true,  // true allows this capability
+        'edit_posts'   => true,
+        'delete_posts' => false, // Use false to explicitly deny
+    )
+);
+if ( null !== $result ) {
+    echo 'Yay! New role created!';
+}
+else {
+    echo 'Oh... the basic_contributor role already exists.';
+}
+print_r($result);
