3c037e648396f4b8c7f52377773cb4b65d9a04d0
nabtron <developer@nabtron.com>
2019-05-11 00:36:12 +0100

    Create output-environment-variables.php

---
+++ output-environment-variables.php
@@ -0,0 +1,10 @@
+<script>
+    console.log("max_execution_time = <?php echo(ini_get('max_execution_time')); ?>");
+    console.log("max_input_time = <?php echo ini_get('max_input_time'); ?>");
+    console.log("memory_limit = <?php echo ini_get('memory_limit'); ?>");
+    console.log("post_max_size = <?php echo ini_get('post_max_size'); ?>");
+    console.log("file_uploads = <?php echo ini_get('file_uploads'); ?>");
+    console.log("upload_max_filesize = <?php echo ini_get('upload_max_filesize'); ?>");
+    console.log("max_file_uploads = <?php echo ini_get('max_file_uploads'); ?>");
+    console.log("default_socket_timeout = <?php echo ini_get('default_socket_timeout'); ?>");
+</script>
